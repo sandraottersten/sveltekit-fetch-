425059f5e734377293b91cd2a5fc7e4ff393f8b5
sandraottersten <otterstensandra@gmail.com>
2021-09-23 10:46:02 +0200

    added simple request

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+
+export async function load({ fetch }) {
+
+  const url = 'https://swapi.dev/api/people/'
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+
+  if (res.ok) {
+    return {
+      props: {
+        data: await res.json()
+      }
+    };
+  }
+
+  return {
+    status: res.status,
+    error: new Error(`Could not load data`)
+  };	
+}
+</script>
+
+<script>
+  export let data
+
+  console.log(data)
+</script>
+
+
+<h1>Hämta från <a href="https://swapi.dev/">https://swapi.dev/</a></h1>
